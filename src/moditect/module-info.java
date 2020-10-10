da41e14fc24849b2d26bb535da100f937740867e
Rob Spoor <rob@icemanx.nl>
2020-10-10 15:01:30 +0200

    Added missing provides clause to module descriptor

--- src/moditect/module-info.java
+++ src/moditect/module-info.java
@@ -2,4 +2,6 @@ module com.github.robtimus.filesystems.memory {
     requires com.github.robtimus.filesystems;
 
     exports com.github.robtimus.filesystems.memory;
+
+    provides java.nio.file.spi.FileSystemProvider with com.github.robtimus.filesystems.memory.MemoryFileSystemProvider;
 }
