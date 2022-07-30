72f375afb968a1b74d7cba48da69f9142d497d8c
Rob Spoor <rob@icemanx.nl>
2022-07-30 16:37:46 +0200

    Use latest parent Added profile for testing as proper module Added GitHub action

--- src/main/java/com/github/robtimus/filesystems/memory/MemoryMessages.java
+++ src/main/java/com/github/robtimus/filesystems/memory/MemoryMessages.java
@@ -27,11 +27,20 @@ import com.github.robtimus.filesystems.UTF8Control;
  */
 final class MemoryMessages {
 
-    private static final String BUNDLE_NAME = "com.github.robtimus.filesystems.memory.fs"; //$NON-NLS-1$
-    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, UTF8Control.INSTANCE);
+    private static final ResourceBundle BUNDLE = getBundle();
 
     private MemoryMessages() {
         throw new IllegalStateException("cannot create instances of " + getClass().getName()); //$NON-NLS-1$
+    }
+
+    private static ResourceBundle getBundle() {
+        final String baseName = "com.github.robtimus.filesystems.memory.fs"; //$NON-NLS-1$
+        try {
+            return ResourceBundle.getBundle(baseName, UTF8Control.INSTANCE);
+        } catch (@SuppressWarnings("unused") UnsupportedOperationException e) {
+            // Java 9 or up; defaults to UTF-8
+            return ResourceBundle.getBundle(baseName);
+        }
     }
 
     private static synchronized String getMessage(String key) {
